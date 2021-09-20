7526619fb0ce6768bf356a25f1f18417fdfe61b4
bekirisgor <bekirisgor@gmail.com>
2021-09-20 12:46:44 +0300

    a

--- Middlewares/UserParser.js
+++ Middlewares/UserParser.js
@@ -1,18 +1,19 @@
 const { getRefreshTokenCookie } = require('../Helpers/requests');
 const { accessTokenParser, jwtDecode } = require('../Helpers/Tokens');
 const { UserService } = require('../Services');
-const { getRefreshTokenById } = require('../Services/User/UserServices');
+const { getRefreshToken } = require('../Services/User/UserServices');
 
 const userParser = async (req, res, next) => {
 	const access_token = accessTokenParser(req);
 
 	req.access_token = access_token;
 	req.refresh_token = getRefreshTokenCookie(req);
+
 	const decodedToken = jwtDecode(access_token);
 	const userID = decodedToken.id;
-	const refreshToken = await getRefreshTokenById(userID);
+	const refreshToken = await getRefreshToken(userID, req.ip, req.useragent);
 	const user = await UserService.getUserbyId(userID).catch(next);
-
+	console.log({ user }, { refreshToken });
 	if (user && refreshToken?.isActive) {
 		req.user = user;
 	} else {
